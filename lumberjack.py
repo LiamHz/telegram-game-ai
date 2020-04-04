7644c940edcff205f3d2e2c37fd78a8742c59f9f
LiamHz <liam.hinzman@gmail.com>
2020-04-04 12:24:16 -0400

    Changed from single line detection to entire screen

--- lumberjack.py
+++ lumberjack.py
@@ -3,57 +3,69 @@ import time
 import numpy as np
 from mss import mss
 from PIL import Image
-from pynput.keyboard import Key, Controller, Listener
+from pynput.keyboard import Key, Controller
 
-nBlackThresh = 8192
-bounding_box = {'top': 420, 'left': 700, 'width': 400, 'height': 130}
-nPixels = bounding_box['width'] * bounding_box['height']
-SAVE_IMAGES = False
+DELAY0 = 0.16
+DELAY1 = 0.02
+nBlackThresh = 4096
+bounding_box = {'top': 130, 'left': 735, 'width': 100, 'height': 370}
+nPixels = bounding_box['width'] * bounding_box['height'] * 4
 
 sct = mss()
 keyboard = Controller()
+og_images = []
 full_images = []
 cropped_images = []
 
+nPartitions = 4
+
 def main():
-    try:
-        while True:
-            # Wait for 180 miliseconds
-            time.sleep(0.18)
+    time.sleep(1)
+    while True:
+        # Wait for animations to complete before screen cap
+        time.sleep(DELAY0)
+        partitions = []
+        command_stack = []
 
-            # Get screen cap
-            screen_cap = np.array(sct.grab(bounding_box))
+        # Get screen cap
+        screen_cap = np.array(sct.grab(bounding_box))
 
-            # Convert screen_cap to black and white with thresholding
-            bw_img = cv2.cvtColor(screen_cap, cv2.COLOR_BGR2GRAY)
-            ret, thresh = cv2.threshold(bw_img, 200, 255, cv2.THRESH_BINARY)
+        # Convert screen_cap to black and white with thresholding
+        bw_img = cv2.cvtColor(screen_cap, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(bw_img, 200, 255, cv2.THRESH_BINARY)
 
-            # Crop image to left half
-            cropped_img = thresh[:, 0:bounding_box['width']//2]
+        # Create a partition for each branch
+        # Inserts from top to bottom ("stack order")
+        partition_height = bounding_box['height'] // nPartitions
+        for i in range(nPartitions):
+            partitions.append(thresh[partition_height*i*2:partition_height*(i+1)*2, :])
 
-            # If number of black pixels in left half is 0, move left
-            nWhite = cv2.countNonZero(cropped_img)
-            nBlack = nPixels - nWhite
+        for i in range(len(partitions)):
+            nWhite = cv2.countNonZero(partitions[i])
+            nBlack = nPixels//nPartitions - nWhite
+
+            # If number of black pixels in the left half of the screen is less
+            # than a threshold, move left
             if nBlack < nBlackThresh:
-                # Press left arrow key
-                keyboard.press(Key.left)
-                print("LEFT")
+                command_stack.append('L')
             else:
-                # Press right arrow key
+                command_stack.append('R')
+
+        for i in range(nPartitions):
+            # Delay is needed to let game handle input
+            time.sleep(DELAY1)
+            command = command_stack.pop()
+            if command == "L":
+                keyboard.press(Key.left)
+                keyboard.release(Key.left)
+                time.sleep(DELAY1)
+                keyboard.press(Key.left)
+                keyboard.release(Key.left)
+            elif command == "R":
                 keyboard.press(Key.right)
-                print("RIGHT")
-
-            if SAVE_IMAGES:
-                cropped_images.append(cropped_img)
-                full_images.append(thresh)
-
-    except KeyboardInterrupt:
-        if SAVE_IMAGES:
-            for i in range(len(cropped_images)):
-                print("{}-crop.bmp".format(i))
-                print("{}-full.bmp".format(i))
-                cv2.imwrite("./{}-crop.bmp".format(i), cropped_images[i])
-                cv2.imwrite("./{}-full.bmp".format(i), full_images[i])
-        cv2.destroyAllWindows()
+                keyboard.release(Key.right)
+                time.sleep(DELAY1)
+                keyboard.press(Key.right)
+                keyboard.release(Key.right)
 
 main()
